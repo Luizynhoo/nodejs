a95277f00c09970865656f1095c5b2c159d9c5a5
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2023-09-25 21:55:50 -0300

    Add files via upload

---
+++ fundamentos/funcoes.js
@@ -0,0 +1,29 @@
+/**
+ * Estudo  das funções
+ */
+
+//função simples
+function hello() {
+    console.log("hello function")
+}
+
+//para executar uma função basta escrever o nome da função com parênteses 
+hello()
+
+//funções atribuídas
+const hello2 = function() {
+    console.log("hello function atribuida")
+
+}
+
+hello2()
+
+//função atribuída simplificada (arrow function)
+const hello3 = () => {
+    console.log("hello arrow function")
+
+}
+
+hello3()
+
+console.log (typeof(hello3))
